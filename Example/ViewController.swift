2f6a336dd118fbff53f23dc3e6ac5acb386c3b97
Ali Kıran <away4m@gmail.com>
2016-11-15 11:37:28 +0300

    swift 3 support

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 import EGMaterialSwitch
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let egSwitch = EGMaterialSwitch(size: .Normal, state: .On)
-        let screenFrame = UIScreen.mainScreen().bounds
-        egSwitch.center = CGPointMake(screenFrame.size.width*6/7, 45)
+        let egSwitch = EGMaterialSwitch(size: .normal, state: .on)
+        let screenFrame = UIScreen.main.bounds
+        
+        egSwitch.center = CGPoint(x:screenFrame.size.width*6/7,y: 45)
         self.view.addSubview(egSwitch)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
